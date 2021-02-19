a8cffc72c5bb3b8e17c7925bbb04f695c5624b2e
moshe-gr <mosegreenberg6@gmail.com>
2021-02-19 08:44:59 +0200

    style fixes

--- src/app/components/live-reports/live-reports.component.ts
+++ src/app/components/live-reports/live-reports.component.ts
@@ -10,30 +10,30 @@ import { SwitchService } from 'src/app/services/switch.service';
 })
 export class LiveReportsComponent implements OnInit{
   idList: string[];
-  test: CoinInfoModel[] = [];
+  liveList: CoinInfoModel[] = [];
   
   constructor(private switchService: SwitchService, private apiService: ApiService) { }
   
   ngOnInit(): void {
     this.switchService.switchList.subscribe(res => this.idList = res);
     for(let i = 0; i < this.idList.length; i++){
-      if(!this.test.find(info => info.id == this.idList[i])){
-        this.apiService.getInfo(this.idList[i]).subscribe(info => this.test.push({id: info.id, pic: info.image.large, cache: info.market_data.current_price.usd, usd: info.market_data.current_price.usd, ils: info.market_data.current_price.ils, eur: info.market_data.current_price.eur}));
+      if(!this.liveList.find(info => info.id == this.idList[i])){
+        this.apiService.getInfo(this.idList[i]).subscribe(info => this.liveList.push({id: info.id, pic: info.image.large, cache: info.market_data.current_price.usd, usd: info.market_data.current_price.usd, ils: info.market_data.current_price.ils, eur: info.market_data.current_price.eur}));
       }
     }
     setInterval(a => {
-      for(let j = 0; j < this.test.length; j++){
-        if(!this.idList.find(id => id == this.test[j].id)){
-          this.test.splice(j, 1);
+      for(let j = 0; j < this.liveList.length; j++){
+        if(!this.idList.find(id => id == this.liveList[j].id)){
+          this.liveList.splice(j, 1);
         }
       }
       for(let i = 0; i < this.idList.length; i++){
-        if(!this.test.find(info => info.id == this.idList[i])){
-          this.apiService.getInfo(this.idList[i]).subscribe(info => this.test.push({id: info.id, pic: info.image.large, cache: info.market_data.current_price.usd, usd: info.market_data.current_price.usd, ils: info.market_data.current_price.ils, eur: info.market_data.current_price.eur}))
+        if(!this.liveList.find(info => info.id == this.idList[i])){
+          this.apiService.getInfo(this.idList[i]).subscribe(info => this.liveList.push({id: info.id, pic: info.image.large, cache: info.market_data.current_price.usd, usd: info.market_data.current_price.usd, ils: info.market_data.current_price.ils, eur: info.market_data.current_price.eur}))
         }
         else{
-          this.apiService.getInfo(this.test[i].id).subscribe(info => {
-            this.test.splice(this.test.indexOf(this.test[i]), 1, {id: info.id, pic: info.image.large, cache: this.test[i].usd, usd: info.market_data.current_price.usd, ils: info.market_data.current_price.ils, eur: info.market_data.current_price.eur})})
+          this.apiService.getInfo(this.liveList[i].id).subscribe(info => {
+            this.liveList.splice(this.liveList.indexOf(this.liveList[i]), 1, {id: info.id, pic: info.image.large, cache: this.liveList[i].usd, usd: info.market_data.current_price.usd, ils: info.market_data.current_price.ils, eur: info.market_data.current_price.eur})})
         }
       }
     }, 10000);
